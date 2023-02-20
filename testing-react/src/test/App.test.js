e84cfb1a24fcbb4e3575bd51162a4ef8fbeddd3d
Raivisto Luka <s2001051@edu.bc.fi>
2023-02-20 13:59:33 +0200

    attempt to make tests
    doesn't work, will fix later

---
+++ testing-react/src/test/App.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from '../App';
+
+test('renders App component without crashing', () => {
+  render(<App />)
+})
+
+test('initial state of memes is an empty array', () => {
+  const { result } = renderHook(() => App())
+  expect(result.current.memes).toEqual([])
+})
+
+test('fetch request updates memes state variable', async () => {
+  const { result, waitForNextUpdate } = renderHook(() => App())
+  await waitForNextUpdate()
+  expect(result.current.memes.length).toBeGreaterThan(0)
+})
+
+test('renders all meme templates from the API', async () => {
+  const { getByAltText } = render(<App />)
+  const response = await axios.get('https://api.imgflip.com/get_memes')
+  const memes = response.data.data.memes
+  memes.forEach(meme => {
+    expect(getByAltText(meme.name)).toBeInTheDocument()
+  })
+})
+
+test('displays the correct title', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('Meme Templates')).toBeInTheDocument()
+})
